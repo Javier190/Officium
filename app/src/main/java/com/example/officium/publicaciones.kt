c75b34410f3fe833583d9a9f78a79a6006069966
Javier190 <62271245+Javier190@users.noreply.github.com>
2020-08-27 11:52:18 -0400

    Se agrego Nueva activity de publicaciones de ofertas

---
+++ app/src/main/java/com/example/officium/publicaciones.kt
@@ -0,0 +1,11 @@
+package com.example.officium
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class publicaciones : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_publicaciones)
+    }
+}
